6d3e4f33313b48a77075267bbb25ca3adc63b451
QuanVOLEMINH <wazzamq1104@gmail.com>
2017-07-03 16:25:34 +0200

    auth with github and get callback

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_github import GitHub
 
 app = Flask(__name__)
+app.config['GITHUB_CLIENT_ID'] = '06078c9479f182a8f8a4'
+app.config['GITHUB_CLIENT_SECRET'] = '06078c9479f182a8f8a4'
+github = GitHub(app)
 
 @app.route('/')
 def index():
     return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+   return github.authorize()
+
+@app.route('/callback')
+def callback():
+    return render_template('callback.html')
+
 
 
 if __name__ == "__main__":

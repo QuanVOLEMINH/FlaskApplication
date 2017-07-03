78b8d75a7c34f3f27adc7f01d81d490435d5db37
QuanVOLEMINH <wazzamq1104@gmail.com>
2017-07-03 21:59:14 +0200

    auth by github and fetch data succesfully

--- app.py
+++ app.py
@@ -1,28 +1,59 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session, url_for
 from flask_github import GitHub
+from requests_oauthlib import OAuth2Session
+from flask.json import jsonify
+import os
+
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
 app = Flask(__name__)
-app.config['GITHUB_CLIENT_ID'] = '06078c9479f182a8f8a4'
-app.config['GITHUB_CLIENT_SECRET'] = '06078c9479f182a8f8a4'
-github = GitHub(app)
+# Information from github app
+client_id = '06078c9479f182a8f8a4'
+client_secret = '4342fac9ab22b88884381cc2b178e16196f4ec3d'
+authorization_base_url = 'https://github.com/login/oauth/authorize'
+token_url = 'https://github.com/login/oauth/access_token'
+
 
 @app.route('/')
 def index():
     return render_template('home.html')
 
+
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/login', methods = ['GET', 'POST'])
+
+@app.route("/login")
 def login():
-   return github.authorize()
+    github = OAuth2Session(client_id)
+    authorization_url, state = github.authorization_url(authorization_base_url)
 
-@app.route('/callback')
+    # State is used to prevent CSRF, keep this for later.
+    session['oauth_state'] = state
+    return redirect(authorization_url)
+
+
+@app.route('/callback', methods=['GET'])
 def callback():
-    return render_template('callback.html')
+    github = OAuth2Session(client_id, state=session['oauth_state'])
+    token = github.fetch_token(token_url, client_secret=client_secret,
+                               authorization_response=request.url)
+    session['oauth_token'] = token
+    return redirect(url_for('.profile'))
 
+
+@app.route("/profile", methods=["GET"])
+def profile():
+    """Fetching a protected resource using an OAuth 2 token.
+    """
+    github = OAuth2Session(client_id, token=session['oauth_token'])
+    data = jsonify(github.get('https://api.github.com/user').json())
+    return jsonify(github.get('https://api.github.com/user').json())
 
 
 if __name__ == "__main__":
+    os.environ['DEBUG'] = "1"
+
+    app.secret_key = os.urandom(24)
     app.run(debug=True)
